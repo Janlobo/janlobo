546f06b45dff5945311c3ed700d6f5aa790d9c34
Jan Lobo <30392984+Janlobo@users.noreply.github.com>
2025-01-30 18:27:28 -0300

    Update Cursos-Certs.js

--- Cursos-Certs.js
+++ Cursos-Certs.js
@@ -1,8 +1,9 @@
+//import fetch from 'node-fetch';
 const fs = require('fs');
 const path = require('path');
 
 async function fetchCertificates() {
-    const fetch = (await import('node-fetch')).default;
+//    const fetch = (await import('node-fetch')).default;
     const response = await fetch('https://www.alura.com.br/api/dashboard/f3f9ff5bce7d04fb3925e9ff8cc54cc49bebbdabeeb43b9861af5d7f9bab821a'); // Substitua pela URL da sua API
     const data = await response.json();
     return data;
